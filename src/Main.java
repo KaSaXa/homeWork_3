960250241635e58d87420174e51b5afc413a852f
KaSaXa <akurmelevv@gmail.com>
2026-07-26 23:52:28 +0300

    task2.1

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+         System.out.printf("Hello and welcome!");
+//task 1
+         int i = 1;
+         byte b = 2;
+         short s = 3;
+         long l = 4;
+         float f = 5f;
+         double d = 6d;
+            System.out.println("Значение переменной " + i + " с типом " + " int " + " равно " + i);
+            System.out.println("Значение переменной " + i + " с типом " + " byte " + " равно " + b);
+            System.out.println("Значение переменной " + i + " с типом " + " short " + " равно " + s);
+            System.out.println("Значение переменной " + i + " с типом " + " long " + " равно " + l);
+            System.out.println("Значение переменной " + i + " с типом " + " float " + " равно " + f);
+            System.out.println("Значение переменной " + i + " с типом " + " double " + " равно " + d);
+
+//task 2
+         double an = 27.12;
+         long ale = 987678965549;
+         double rus = 2.786;
+         int main = 569;
+         int ileas = -159;
+         byte brus = 27897;
+         byte old = 67;
+//task 3
+
+    }
+    }
